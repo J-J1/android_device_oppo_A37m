1a4b4389bdd75e286f13a624bc58d3c4e6653bbf
Cyzi7777 <m13045350508@163.com>
2022-03-03 08:37:14 +0800

    Third

--- omni_A37.mk
+++ omni_A37.mk
@@ -22,9 +22,6 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from A37 device
-$(call inherit-product, device/oppo/A37/device.mk)
-
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
